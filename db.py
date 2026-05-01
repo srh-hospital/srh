1a2ed4af6b33b01a713eeace7732e6c12710cbea
srh-hospital <jaredasunto20@gmail.com>
2026-05-02 01:42:49 +0800

    db.py

--- db.py
+++ db.py
@@ -1,21 +1,14 @@
+import os
 import mysql.connector
 from flask import g
-import os
-
-DB_CONFIG = {
-    "host":     os.getenv("DB_HOST", "localhost"),
-    "port":     int(os.getenv("DB_PORT", 3306)),
-    "user":     os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "yourpassword"),
-    "database": os.getenv("DB_NAME", "saint_raphael_hospital"),
-}
 
 def get_db():
-    if "db" not in g:
-        g.db = mysql.connector.connect(**DB_CONFIG)
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host=os.environ.get('DB_HOST', 'localhost'),
+            user=os.environ.get('DB_USER', 'root'),
+            password=os.environ.get('DB_PASSWORD', ''),
+            database=os.environ.get('DB_NAME', 'rfid_db'),
+            port=int(os.environ.get('DB_PORT', 3306))
+        )
     return g.db
-
-def close_db(e=None):
-    db = g.pop("db", None)
-    if db is not None and db.is_connected():
-        db.close()
